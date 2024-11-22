70706b2237dd09341b712700d80a74853d1d2a5a
Saidazimov Nurbek <saidazimovnurbek01@gmail.com>
2024-11-22 09:45:55 +0500

    usesubject

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,2 +1,4 @@
 import React from "react";
 import { AppBar, Toolbar, Typography, Box, Select, MenuItem } from "@mui/material";
+import { UseDispatch } from "react-redux";
+import {useSubject} from '../features/search/searchSlice';
