ebb0bf2b9e93ae7ddc46f0e23e7e3db55e908eff
Saidazimov Nurbek <saidazimovnurbek01@gmail.com>
2024-11-22 18:22:39 +0500

    dispatch to the tsx

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,2 +1,25 @@
-import React from "react";
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { setQuery } from '../features/search/searchSlice';
+import { RootState } from '../store';
+import { TextField, Button, Box } from '@mui/material';
+
+const SearchBar: React.FC = () => {
+  const dispatch = useDispatch();
+  const query = useSelector((state: RootState) => state.search.query);
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setQuery(event.target.value));
+  };
+
+  return (
+    <Box sx={{ display: 'flex', gap: 2, my: 2 }}>
+      <TextField fullWidth label="Search Media" value={query} onChange={handleSearch} />
+      <Button variant="contained" onClick={() => dispatch(setQuery(''))}>
+        Reset
+      </Button>
+    </Box>
+  );
+};
+
+export default SearchBar;
