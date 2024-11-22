ee856950901e8445ef7c41d11c8cfcc770f4fc49
Saidazimov Nurbek <saidazimovnurbek01@gmail.com>
2024-11-22 12:12:56 +0500

    searchslice test

--- src/features/search/searchSlice.ts
+++ src/features/search/searchSlice.ts
@@ -5,3 +5,23 @@ interface SearchState{
     query: string;
 }
 
+const initialState: SearchState = {
+    subject: '',
+    query: '',
+  };
+
+  const searchSlice = createSlice({
+    name: 'search',
+    initialState,
+    reducers: {
+      setSubject: (state, action: PayloadAction<string>) => {
+        state.subject = action.payload;
+      },
+      setQuery: (state, action: PayloadAction<string>) => {
+        state.query = action.payload;
+      },
+    },
+  });
+  
+  export const { setSubject, setQuery } = searchSlice.actions;
+  export default searchSlice.reducer;
