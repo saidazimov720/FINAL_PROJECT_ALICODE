08798766a81d15d701dc8911fdc5f82fd3406d3a
Saidazimov Nurbek <saidazimovnurbek01@gmail.com>
2024-11-22 18:26:12 +0500

    edit constant ts

---
+++ src/utils/constant.ts
@@ -0,0 +1,12 @@
+
+export const API_BASE_URL = 'https://api.mocked-media-platform.com';
+export const MEDIA_ENDPOINT = '/media';
+export const DOWNLOAD_ENDPOINT = '/download';
+
+export const APP_NAME = 'Edu-Media Platform';
+export const FOOTER_TEXT = 'All rights reserved.';
+export const NO_MEDIA_MESSAGE = 'No media found. Please adjust your search filters.';
+
+
+export const DEFAULT_SUBJECT = 'All';
+export const DEFAULT_QUERY = '';
