e704d99fdfa3d3934ed6a2bb9c213f47486da90b
Saidazimov Nurbek <saidazimovnurbek01@gmail.com>
2024-11-22 18:24:17 +0500

    searchSlice ts

--- src/features/search/searchSlice.ts
+++ src/features/search/searchSlice.ts
@@ -1,27 +1,27 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-interface SearchState{
-    subject: string;
-    query: string;
+interface SearchState {
+  subject: string;
+  query: string;
 }
 
 const initialState: SearchState = {
-    subject: '',
-    query: '',
-  };
+  subject: 'All',
+  query: '',
+};
 
-  const searchSlice = createSlice({
-    name: 'search',
-    initialState,
-    reducers: {
-      setSubject: (state, action: PayloadAction<string>) => {
-        state.subject = action.payload;
-      },
-      setQuery: (state, action: PayloadAction<string>) => {
-        state.query = action.payload;
-      },
+const searchSlice = createSlice({
+  name: 'search',
+  initialState,
+  reducers: {
+    setSubject: (state, action: PayloadAction<string>) => {
+      state.subject = action.payload;
     },
-  });
-  
-  export const { setSubject, setQuery } = searchSlice.actions;
-  export default searchSlice.reducer;
+    setQuery: (state, action: PayloadAction<string>) => {
+      state.query = action.payload;
+    },
+  },
+});
+
+export const { setSubject, setQuery } = searchSlice.actions;
+export default searchSlice.reducer;
